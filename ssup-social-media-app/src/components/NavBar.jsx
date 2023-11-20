2467bbddf0d3ef3a1091c123e0fe60044bbd6738
davidgit3000 <davidkarik5@gmail.com>
2023-11-19 18:21:30 -0800

    update the profile menu on the navigation bar

--- ssup-social-media-app/src/components/NavBar.jsx
+++ ssup-social-media-app/src/components/NavBar.jsx
@@ -8,10 +8,20 @@ import {
   Button,
   Grid,
   CssBaseline,
+  Avatar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon,
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import SearchIcon from "@mui/icons-material/Search";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
 import logo from "../assets/logo.png";
+import { deepOrange } from "@mui/material/colors";
+import { useState } from "react";
 
 const theme = createTheme({
   palette: {
@@ -25,6 +35,15 @@ const theme = createTheme({
 });
 
 export default function NavBar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -96,14 +115,81 @@ export default function NavBar() {
                 </Button>
               </div>
               <div className="mx-1">
-                <Button variant="text" color="nav_link">
-                  Profile
-                </Button>
+                <IconButton
+                  onClick={handleClick}
+                  size="small"
+                  sx={{ ml: 2 }}
+                  aria-controls={open ? "account-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                >
+                  {" "}
+                  <Avatar
+                    sx={{ width: 32, height: 32, bgcolor: deepOrange[500] }}
+                  >
+                    DL
+                  </Avatar>
+                </IconButton>
               </div>
             </Toolbar>
           </Container>
         </AppBar>
       </ThemeProvider>
+
+      {/** Source: https://mui.com/material-ui/react-menu/ - Account menu */}
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar sx={{ width: 32, height: 32, bgcolor: deepOrange[500] }} />
+          My Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon fontSize="small">
+            <Logout />
+          </ListItemIcon>
+          Log out
+        </MenuItem>
+      </Menu>
     </>
   );
 }
